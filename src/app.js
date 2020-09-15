d8bf1648b868b2e80ebdab271ee54604ae7fda11
David Royer <droyer01@gmail.com>
2020-09-15 00:37:44 -0400

    feat: adds human grid item

--- src/app.js
+++ src/app.js
@@ -1,22 +1,21 @@
 /* eslint-disable no-unused-vars */
 
 import { DinoConstructor, HumanConstructor } from "./models";
+import { insertIntoArray } from "./helpers/";
 
 const getApiData = async () => {
   const response = await fetch("data/dino.json");
   return await response.json();
 };
 
-async function loadGrid() {
+async function loadGrid(human) {
   const { Dinos } = await getApiData();
-  const dinosArray = Dinos.map((dino) => new DinoConstructor(dino));
-  console.log("dinosArray", dinosArray);
-
+  const creaturesArray = Dinos.map((dino) => new DinoConstructor(dino));
   const grid = document.getElementById("grid");
 
-  dinosArray.forEach((dino) => {
-    dino.dinoMethod();
-    dino.creatureMethod();
+  insertIntoArray(creaturesArray, 4, human);
+
+  creaturesArray.forEach((dino) => {
     let gridItem = document.createElement("div");
     gridItem.textContent = dino.species;
     gridItem.classList.add("grid-item");
@@ -24,20 +23,19 @@ async function loadGrid() {
   });
 }
 
-loadGrid();
-
 function handleButtonClick() {
   const submitBtn = document.getElementById("submit-button");
 
   submitBtn.addEventListener("click", function () {
-    let user = new HumanConstructor({
+    let human = new HumanConstructor({
       name: "David",
       height: "5",
       weight: 200,
       diet: "Herbivore",
+      species: "human",
     });
-    user.humanMethod();
-    user.creatureMethod();
+
+    loadGrid(human);
   });
 }
 
--- src/helpers/index.js
+++ src/helpers/index.js
@@ -2,3 +2,8 @@ export function inheritFromParent(Child, Parent) {
   Child.prototype = Object.create(Parent.prototype);
   Child.prototype.constructor = Child;
 }
+
+export function insertIntoArray(array, insertPosition, insertValue) {
+  array.splice(insertPosition, 0, insertValue);
+  // return [...array].splice(insertPosition, 0, insertValue); // Maybe for immutability
+}
