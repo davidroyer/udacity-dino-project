41ff232ab956175c592ccf71f9170e61c47a7c23
David Royer <droyer01@gmail.com>
2020-09-29 19:21:00 -0400

    feat(card): adds images

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,11 +1,16 @@
+// import PigeonImage from "../images/pigeon.png";
+
 const GridItemContent = (item) => {
   function factRenderer(item) {
     if (item.species !== "human") return `<p>${item.fact}</p>`;
     else return "";
   }
 
+  const imgName = item.species.toLowerCase();
+
   return `
   <h2 class="text-3xl">${item.species}</h2>
+  <img src="data/images/${imgName}.png" alt="${item.species} image"/>
   ${factRenderer(item)}
   `;
 };
