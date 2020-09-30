4418a2ad3fb88a353cb3e7b50d862e21ae660cc3
David Royer <droyer01@gmail.com>
2020-09-30 06:39:11 -0400

    chore: cleanup

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,3 @@
-// import PigeonImage from "../images/pigeon.png";
-
 const GridItemContent = (item) => {
   function factRenderer(item) {
     if (item.species !== "human") return `<p>${item.fact}</p>`;
